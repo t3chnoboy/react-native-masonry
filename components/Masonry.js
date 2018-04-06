bc2db13767edb3d354235e8a2b385769cdcb84cc
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2018-04-07 00:29:14 +0300

    ooops

--- components/Masonry.js
+++ components/Masonry.js
@@ -59,11 +59,12 @@ export default class Masonry extends Component {
 	}
 
 	componentWillReceiveProps(nextProps) {
-   	// Check if it's array and contains more than 1 item
-    	if (!Array.isArray(nextProps.bricks) || nextProps.bricks.length === 0) {
-      		this.setState(state => ({
-           		dataSource: state.dataSource.cloneWithRows([])       
-       		}));
+   		// Check if it's array and contains more than 1 item
+    		if (!Array.isArray(nextProps.bricks) || nextProps.bricks.length === 0) {
+      			this.setState(state => ({
+           			dataSource: state.dataSource.cloneWithRows([])       
+       			}));
+		}
     	}
 
 	resolveBricks({ bricks, columns }, partiallyCache = false) {
